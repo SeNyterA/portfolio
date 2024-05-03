baad023bbe3b9f3293eda8af0ddabc1cea0dcf2e
Nguyễn Đức Khang <121739530+SeNyterA@users.noreply.github.com>
2024-05-03 09:22:46 +0700

    init & move portfolio from live-workpace

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import Portfolio from "./portfolio/Portfolio.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>
+);
